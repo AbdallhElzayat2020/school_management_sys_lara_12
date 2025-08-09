aa3c61df927a6e10fdd0bfd498af9b680cba35e4
Abdallh ELzayat <abdallhelzayat900@gmail.com>
2025-08-09 23:34:38 +0300

    Add SCSS components for accordion, button, helpers, and layout styles

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,56 +4,52 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="keywords" content="HTML5 Template" />
-    <meta name="description" content="Webmin - Bootstrap 4 & Angular 5 Admin Dashboard Template" />
-    <meta name="author" content="potenzaglobalsolutions.com" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="keywords" content="HTML5 Template"/>
+    <meta name="description" content="Webmin - Bootstrap 4 & Angular 5 Admin Dashboard Template"/>
+    <meta name="author" content=""/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
     @include('layouts.head')
 </head>
 
 <body>
 
-    <div class="wrapper">
-
-        <!--=================================
- preloader -->
-
-        <div id="pre-loader">
-            <img src="assets/images/pre-loader/loader-01.svg" alt="">
-        </div>
-
-        <!--=================================
- preloader -->
-
-        @include('layouts.main-header')
-
-        @include('layouts.main-sidebar')
-
-        <!--=================================
- Main content -->
-        <!-- main-content -->
-        <div class="content-wrapper">
-
-            @yield('page-header')
-
-            @yield('content')
-
-            <!--=================================
- wrapper -->
-
-            <!--=================================
- footer -->
-
-            @include('layouts.footer')
-        </div><!-- main content wrapper end-->
-    </div>
-    </div>
-    </div>
+<div class="wrapper">
 
     <!--=================================
- footer -->
+preloader -->
 
-    @include('layouts.footer-scripts')
+    <div id="pre-loader">
+        <img src="assets/images/pre-loader/loader-01.svg" alt="">
+    </div>
+
+    <!--================================= preloader ================================= -->
+
+    @include('layouts.main-header')
+
+    @include('layouts.main-sidebar')
+
+    <!--=================================Main content ================================= -->
+    <!-- main-content -->
+    <div class="content-wrapper">
+
+        @yield('page-header')
+
+        @yield('content')
+
+        <!--================================= wrapper ================================= -->
+
+        <!--================================= footer ================================= -->
+
+        @include('layouts.footer')
+    </div>
+    <!-- main content wrapper end-->
+</div>
+</div>
+</div>
+
+<!--================================= footer ================================= -->
+
+@include('layouts.footer-scripts')
 
 </body>
 
