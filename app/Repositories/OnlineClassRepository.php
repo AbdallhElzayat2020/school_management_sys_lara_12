1baee14bd66ab1c2fd1cf9ae8b63a88fd83b669c
Abdallh ELzayat <abdallhelzayat900@gmail.com>
2025-09-02 05:10:13 +0300

    feat: integrate local database actions with Zoom meeting operations
    - Enhanced `OnlineClassRepository` to sync database records with Zoom meeting creation, updates, and deletions.
    - Updated `ZoomService` to streamline meeting-related responses and remove redundant logic.
    - Improved resource management by linking the ` OnlineClass ` model directly to Zoom operations.

--- app/Repositories/OnlineClassRepository.php
+++ app/Repositories/OnlineClassRepository.php
@@ -28,12 +28,46 @@ class OnlineClassRepository implements OnlineClassInterface
 
     public function store($request)
     {
-        return $this->zoomServices->store($request);
+        $online_classes = $this->zoomServices->store($request);
+
+        OnlineClass::create([
+            'grade_id' => $request->grade_id,
+            'classroom_id' => $request->classroom_id,
+            'section_id' => $request->section_id,
+            'user_id' => auth()->user()->id,
+            'template_id' => $online_classes->id,
+            'topic' => $online_classes->topic,
+            'start_time' => $online_classes->start_time,
+            'duration' => $online_classes->duration,
+            'password' => $online_classes->password,
+        ]);
+
+        return response()->json([
+            'online_class' => new OnlineClassResource($online_classes),
+        ], 201);
     }
 
     public function update($id, $request)
     {
-        return $this->zoomServices->update($id, $request);
+
+        // update from Zoom
+        $online_class = $this->zoomServices->update($id, $request);
+
+        // update in DB
+        $meeting = OnlineClass::findOrFail($id);
+        $meeting->update([
+            'grade_id' => $request->grade_id,
+            'classroom_id' => $request->classroom_id,
+            'section_id' => $request->section_id,
+            'template_id' => $online_class->id,
+            'topic' => $request->topic,
+            'start_time' => $request->start_time,
+            'duration' => $request->duration,
+        ]);
+
+        return response()->json([
+            'message' => 'Online class updated successfully.',
+        ]);
     }
 
     public function destroy($id)
--- app/Services/ZoomService.php
+++ app/Services/ZoomService.php
@@ -3,17 +3,14 @@
 namespace App\Services;
 
 use App\Http\Resources\OnlineClassResource;
+use App\Models\OnlineClass;
 use Jubaer\Zoom\Facades\Zoom;
 
 class ZoomService
 {
     public function store($request)
     {
-        $online_classes = Zoom::createMeeting([
-            'grade_id' => $request->input('grade_id'),
-            'classroom_id' => $request->input('classroom_id'),
-            'section_id' => $request->input('section_id'),
-            'user_id' => $request->input('user_id'),
+        return Zoom::createMeeting([
             'template_id' => $request->input('template_id'),
             'topic' => $request->input('topic'),
             'start_time' => $request->input('start_time'),
@@ -30,32 +27,18 @@ class ZoomService
                 'approval_type' => 0, // 0 => Automatically Approve, 1 => Manually Approve, 2 => No Registration Required
             ],
         ]);
-
-
-        return response()->json([
-            'online_class' => new OnlineClassResource($online_classes),
-        ], 201);
     }
-
 
 
     public function update($id, $request)
     {
         $meetingId = Zoom::getMeeting($id);
-        Zoom::updateMeeting($meetingId, [
-            'grade_id' => $request->input('grade_id'),
-            'classroom_id' => $request->input('classroom_id'),
-            'section_id' => $request->input('section_id'),
-            'user_id' => $request->input('user_id'),
+        return Zoom::updateMeeting($meetingId, [
             'template_id' => $request->input('template_id'),
             'topic' => $request->input('topic'),
             'start_time' => $request->input('start_time'),
             'duration' => $request->input('duration'),
             'password' => $request->input('password'),
-        ]);
-
-        return response()->json([
-            'message' => 'Online class updated successfully.',
         ]);
     }
 
@@ -64,6 +47,8 @@ class ZoomService
     {
         $meetingId = Zoom::getMeeting($id);
         Zoom::deleteMeeting($meetingId);
+
+        OnlineClass::destroy($id);
 
         return response()->json([
             'message' => 'Online class deleted successfully.',
